30f777f30543cf4bf861e90ccfa6ad91016ac215
Ludvig Hult <ludvig.hult@gmail.com>
2022-03-09 08:26:33 +0100

    wrote a Haskell wrapper for LSODA that works on the example problem

--- LSODA.hs
+++ LSODA.hs
@@ -1,11 +1,27 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 
 module LSODA where
+
+import Control.Monad
 import Foreign.Marshal.Array
+import Foreign.Marshal.Utils
 import Foreign.Ptr
 import Foreign.StablePtr
-import Foreign.Marshal.Utils
 import Foreign.Storable
+import System.IO.Unsafe
+import Text.Printf
+
+-- | The right hand side function f in a ODE
+-- y' = f(t,y)
+type RHS =
+  -- | The dimensionality of f
+  Int ->
+  -- | t
+  Double ->
+  -- | y
+  [Double] ->
+  -- | y'
+  [Double]
 
 -- | A F-fun computes the right hand side in a ODE
 -- It is a subroutine. I.e. returns void.
@@ -23,23 +39,23 @@ type FFun =
 -- | a subroutine to compute the jacobian of the thing
 -- jac (neq, t, y, ml, mu, pd, nrowpd)
 type JacFun =
-    -- | `neq` the number of equations
-    Ptr Int ->
-    -- | `t` the number of equations
-    Ptr Double ->
-    -- | `y` the current state
-    Ptr Double ->
-    -- | `ml` 
-    Ptr Double ->
-    -- | `mu` 
-    Ptr Double ->
-    -- | `pd` the vector to fill with partial derivatives. an array representing a matrix with `nrowpd` rows, and `neq` columns
-    -- Dont forget that fortran is column major! so entry [k `neq` + j] is the kth column, jth row
-    -- The exact format depends on the jacobian type passed to @lsoda'
-    Ptr Double ->
-    -- | `nrowpd` the number of equations
-    Ptr Int ->
-    ()
+  -- | `neq` the number of equations
+  Ptr Int ->
+  -- | `t` the number of equations
+  Ptr Double ->
+  -- | `y` the current state
+  Ptr Double ->
+  -- | `ml`
+  Ptr Double ->
+  -- | `mu`
+  Ptr Double ->
+  -- | `pd` the vector to fill with partial derivatives. an array representing a matrix with `nrowpd` rows, and `neq` columns
+  -- Dont forget that fortran is column major! so entry [k `neq` + j] is the kth column, jth row
+  -- The exact format depends on the jacobian type passed to @lsoda'
+  Ptr Double ->
+  -- | `nrowpd` the number of equations
+  Ptr Int ->
+  ()
 
 foreign import ccall safe "lsoda_"
   lsoda' ::
@@ -81,4 +97,110 @@ foreign import ccall safe "lsoda_"
     IO ()
 
 foreign import ccall "wrapper" wrapFFun :: FFun -> IO (FunPtr FFun)
+
 foreign import ccall "wrapper" wrapJacFun :: JacFun -> IO (FunPtr JacFun)
+
+data LSODARes = LSODARes
+  { ys :: [[Double]],
+    ts :: [Double],
+    success :: Bool,
+    msg :: String,
+    noStepsTaken :: Int,
+    noFs :: Int,
+    noJs :: Int,
+    lastMethodUsed :: Int,
+    lastSwitchedAt :: Double
+  }
+  deriving (Show)
+
+res0 =
+  LSODARes
+    { ys = [],
+      ts = [],
+      success = True,
+      msg = "Not even started",
+      noStepsTaken = 0,
+      noFs = 0,
+      noJs = 0,
+      lastMethodUsed = -1,
+      lastSwitchedAt = -1
+    }
+
+fprimWrapper :: RHS -> FFun
+fprimWrapper fprim neqPtr tPtr yPtr yDotPtr = do
+  neq <- peek neqPtr
+  y <- peekArray neq yPtr
+  t <- peek tPtr
+  pokeArray yDotPtr $ fprim neq t y
+
+-- | a simplified LSODA API for my specific use case :)
+simpLsoda ::
+  -- | The right hand side in the ODE
+  RHS ->
+  -- | The initial state
+  [Double] ->
+  -- | the time steps T for which to report results
+  [Double] ->
+  -- | returns
+  -- left = error message
+  -- right = the trajectory at the times t
+  LSODARes
+{-# NOINLINE simpLsoda #-}
+simpLsoda ffun y0 ts = unsafePerformIO $ do
+  let fex = fprimWrapper ffun
+  if length ts <= 2
+    then return $ res0 {msg = "Too short time vector", success = False, ys = [], ts = []}
+    else simpLsodaAux fex y0 ts
+
+-- PRE length ts >= 2
+simpLsodaAux :: FFun -> [Double] -> [Double] -> IO LSODARes
+simpLsodaAux ffun y0 ts = do
+  let neqVal = length y0
+  let lrwVal = 70 -- this work array size must be adjusted somehow.
+  let liwVal = 23 -- Im not sure if this value can ALWAYS be 23
+  fPtr <- wrapFFun ffun
+  neqPtr <- new neqVal
+  iWorkPtr <- newArray $ replicate liwVal 0
+  rWorkPtr <- newArray $ replicate lrwVal 0
+  yPtr <- newArray y0
+  tPtr <- new $ head ts
+  tOutPtr <- new 0.4
+  iTolPtr <- new 2
+  rTolPtr <- new 1e-4
+  aTolPtr <- newArray [1e-6, 1e-10, 1e-6]
+  iTaskPtr <- new 1
+  iStatePtr <- new 1
+  iOptPtr <- new 0
+  lrwPtr <- new lrwVal
+  liwPtr <- new liwVal
+  jtPtr <- new 2
+  let jacDummyPtr = nullFunPtr -- since I use jt=2, the jacobian can be a dummy argument. e.g. a null pointer
+  -- step1
+  -- PRE. initialize with success = True
+  let step1 :: LSODARes -> [Double] -> IO LSODARes
+      step1 res@LSODARes {success = False} _ = return res
+      step1 res@LSODARes {success = True} [] = return res {msg = "Finished!"}
+      step1 res@LSODARes {ys = yOuts, ts = tsThisFar} (t : ts) = do
+        poke tOutPtr t -- set the new target time
+        -- do the solving
+        lsoda' fPtr neqPtr yPtr tPtr tOutPtr iTolPtr rTolPtr aTolPtr iTaskPtr iStatePtr iOptPtr rWorkPtr lrwPtr iWorkPtr liwPtr jacDummyPtr jtPtr
+        -- find out how it went
+        iState <- peek iStatePtr
+        t <- peek tPtr
+        yNew <- peekArray neqVal yPtr
+        if iState < 0
+          then return res {msg = printf "Stopped. istate =%d at t=%f" iState t, success = False}
+          else step1 res {ys = yOuts ++ [yNew], ts = tsThisFar ++ [t], msg = "Completed a step"} ts
+
+  finalVal <- step1 res0 {ys = [], ts = [], success = True, msg = ""} ts
+
+  rwork <- peekArray lrwVal rWorkPtr
+  iwork <- peekArray liwVal iWorkPtr
+  return
+    finalVal
+      { noStepsTaken = iwork !! 10,
+        noFs = iwork !! 11,
+        noJs = iwork !! 12,
+        lastMethodUsed = iwork !! 18,
+        lastSwitchedAt = rwork !! 14
+      }
--- Main.hs
+++ Main.hs
@@ -1,69 +1,31 @@
-
-import Data.Int
-
-import Text.Printf
-import Control.Monad
-
-import Foreign.Marshal.Array
-import Foreign.Ptr
-import Foreign.StablePtr
-import Foreign.Marshal.Utils
-import Foreign.Storable
 import LSODA
+import Text.Printf
 
 main :: IO ()
 main = do
   print "lsoda example:"
   runLSODA
 
-
-fex :: FFun
-fex neqPtr tPtr yPtr yDotPtr= do 
-    [y0,y1,y2] <- peekArray 3 yPtr
-    let ydot0 = -0.4E0*y0 + 1.0E4*y1*y2;
-        ydot2 = 3.0e7*y1*y1;
-        ydot1 = -ydot0 - ydot2;
-    pokeArray yDotPtr [ydot0,ydot1,ydot2]
+-- PRE y has length 3
+-- TODO move to some sized vector type
+fprim :: RHS
+fprim neq t y = 
+    let y0:y1:y2:_ = y
+        ydot0 = -0.4E0 * y0 + 1.0E4 * y1 * y2
+        ydot2 = 3.0e7 * y1 * y1
+        ydot1 = -ydot0 - ydot2
+    in [ydot0, ydot1, ydot2]
 
 
 runLSODA :: IO ()
 runLSODA = do
-    let neqVal = 3
-    let lrwVal = 70
-    let liwVal = 23
-    fPtr <- wrapFFun fex
-    neqPtr <- new neqVal
-    iWorkPtr <- newArray $ replicate liwVal 0
-    rWorkPtr <- newArray $ replicate lrwVal 0
-    yPtr <- newArray [1,0,0]
-    tPtr <- new 0
-    tOutPtr <- new 0.4
-    iTolPtr <- new 2
-    rTolPtr <- new 1e-4
-    aTolPtr <- newArray [1e-6,1e-10,1e-6]
-    iTaskPtr <- new 1
-    iStatePtr <- new 1
-    iOptPtr <- new 0
-    lrwPtr <- new lrwVal
-    liwPtr <- new liwVal
-    jtPtr <- new 2
-    let jacDummyPtr =nullFunPtr  -- since I use jt=2, the jacobian can be a dummy argument. e.g. a null pointer
-    let step1 k = do
-        lsoda' fPtr neqPtr yPtr tPtr tOutPtr iTolPtr rTolPtr aTolPtr iTaskPtr iStatePtr iOptPtr rWorkPtr lrwPtr iWorkPtr liwPtr jacDummyPtr jtPtr
-        iState <- peek iStatePtr
-        t <- peek tPtr
-        tOut <- peek tOutPtr
-        [y1,y2,y3] <- peekArray neqVal yPtr
-        printf "at t = %-12.4e  y = %14.6e  %14.6e  %14.6e\n" t y1 y2 y3
-        if iState < 0
-            then printf "error halt.. istate =%d" iState
-            else poke tOutPtr (tOut*10)
-        when (k <= 12) $ step1 (k+1)
-
-    step1 1
-
-    rwork <- peekArray lrwVal rWorkPtr
-    iwork <- peekArray liwVal iWorkPtr
-    putStrLn ""
-    printf "no. steps =%4d  no. f-s =%4d  no. j-s =%3d\nmethod last used =%2d last switch was at t =%e\n" (iwork !! 10) ( iwork !!11)( iwork !!12)( iwork !!18) (rwork !!14)
-
+  let ts = [4 * 10 ** m | m <- [-1 .. 11]]
+  let res = simpLsoda fprim [1, 0, 0] ts
+  let LSODARes {success = didSucceed, ts = ts, ys = ys} = res
+  if didSucceed
+    then
+      ( do
+          mapM_ (\(t, y) -> printf "at t = %-12.4e  y = %14.6e  %14.6e  %14.6e\n" t (head y) (y !! 1) (y !! 2)) $ zip ts ys
+          printf "no. steps =%4d  no. f-s =%4d  no. j-s =%3d\nmethod last used =%2d last switch was at t =%e\n" (noStepsTaken res) (noFs res) (noJs res) (lastMethodUsed res) (lastSwitchedAt res)
+      )
+    else print "Failed solving"
